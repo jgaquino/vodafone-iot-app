9c95fb848996d8127f96b9a1c86b1e55e708ff7e
Gabriel <iamjoaoaquino@gmail.com>
2024-12-13 10:49:30 +0100

    The `Topbar` component has been created + Vodafone PNG logo has been added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <Topbar />
+    </main>
+  );
 }
+
+const Topbar = () => (
+  <nav className="flex items-center bg-white px-6 py-4">
+    <Image
+      src="/logo.png"
+      alt="Vodafone logo"
+      width={35}
+      height={35}
+      priority
+    />
+    <ul className="flex gap-2 ml-10 text-black">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/devices">Devices</Link>
+      </li>
+    </ul>
+  </nav>
+);
