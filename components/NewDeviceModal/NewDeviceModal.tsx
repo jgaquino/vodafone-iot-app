dfc0d20be13a8fd5b2938f3eb2635f1787599676
Gabriel <iamjoaoaquino@gmail.com>
2024-12-14 13:37:12 +0100

    The `validateForm` function has been created to validate the `device` form in `NewDeviceModal` component.

--- components/NewDeviceModal/NewDeviceModal.tsx
+++ components/NewDeviceModal/NewDeviceModal.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState, type ChangeEvent, type FormEvent } from "react";
 import * as Dialog from "toldo";
 import type Device from "@/types/Device";
+import validateForm from "./validateForm";
 
 type NewDeviceModalProps = {
   isOpen: boolean;
@@ -13,6 +14,13 @@ const NewDeviceModal = ({
   onAddDevice,
 }: NewDeviceModalProps) => {
   const [newDevice, setNewDevice] = useState(DEFAULT_DEVICE);
+  const [errors, setErrors] = useState({
+    name: "",
+    mobileNumber: "",
+    lastConnection: "",
+    latitude: "",
+    longitude: "",
+  });
 
   const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -22,11 +30,16 @@ const NewDeviceModal = ({
   const handleSubmit = useCallback(
     (e: FormEvent) => {
       e.preventDefault();
+
+      const { newErrors, isValid } = validateForm(errors, newDevice);
+      setErrors(newErrors);
+      if (!isValid) return;
+
       onAddDevice(newDevice);
       setNewDevice(DEFAULT_DEVICE);
       onClose();
     },
-    [newDevice]
+    [newDevice, errors]
   );
 
   return (
@@ -46,31 +59,48 @@ const NewDeviceModal = ({
                   name="name"
                   value={newDevice.name}
                   onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md"
-                  required
+                  className={`w-full px-3 py-2 border rounded-md ${
+                    errors.name ? "border-red-500" : ""
+                  }`}
                 />
+                {errors.name && (
+                  <span className="text-red-500 text-sm">{errors.name}</span>
+                )}
               </div>
               <div className="mb-4">
                 <label className="block mb-1">Mobile Number</label>
+                <p className="text-xs">Format +00123456789</p>
                 <input
                   type="text"
                   name="mobileNumber"
                   value={newDevice.mobileNumber}
                   onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md"
-                  required
+                  className={`w-full px-3 py-2 border rounded-md ${
+                    errors.mobileNumber ? "border-red-500" : ""
+                  }`}
                 />
+                {errors.mobileNumber && (
+                  <span className="text-red-500 text-sm">
+                    {errors.mobileNumber}
+                  </span>
+                )}
               </div>
               <div className="mb-4">
                 <label className="block mb-1">Last Connection</label>
                 <input
-                  type="text"
+                  type="date"
                   name="lastConnection"
                   value={newDevice.lastConnection}
                   onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md"
-                  required
+                  className={`w-full px-3 py-2 border rounded-md ${
+                    errors.lastConnection ? "border-red-500" : ""
+                  }`}
                 />
+                {errors.lastConnection && (
+                  <span className="text-red-500 text-sm">
+                    {errors.lastConnection}
+                  </span>
+                )}
               </div>
               <div className="mb-4">
                 <label className="block mb-1">Latitude</label>
@@ -79,9 +109,15 @@ const NewDeviceModal = ({
                   name="latitude"
                   value={newDevice.latitude}
                   onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md"
-                  required
+                  className={`w-full px-3 py-2 border rounded-md ${
+                    errors.latitude ? "border-red-500" : ""
+                  }`}
                 />
+                {errors.latitude && (
+                  <span className="text-red-500 text-sm">
+                    {errors.latitude}
+                  </span>
+                )}
               </div>
               <div className="mb-4">
                 <label className="block mb-1">Longitude</label>
@@ -90,9 +126,15 @@ const NewDeviceModal = ({
                   name="longitude"
                   value={newDevice.longitude}
                   onChange={handleInputChange}
-                  className="w-full px-3 py-2 border rounded-md"
-                  required
+                  className={`w-full px-3 py-2 border rounded-md ${
+                    errors.longitude ? "border-red-500" : ""
+                  }`}
                 />
+                {errors.longitude && (
+                  <span className="text-red-500 text-sm">
+                    {errors.longitude}
+                  </span>
+                )}
               </div>
               <div className="flex justify-end space-x-2">
                 <button
---
+++ components/NewDeviceModal/validateForm.ts
@@ -0,0 +1,50 @@
+import Device from "@/types/Device";
+
+const validateForm = (errors: Device, newDevice: Device) => {
+  const newErrors = { ...errors };
+  let isValid = true;
+
+  if (!newDevice.name) {
+    newErrors.name = "Name is required.";
+    isValid = false;
+  } else {
+    newErrors.name = "";
+  }
+
+  if (!mobileNumberRegex.test(newDevice.mobileNumber)) {
+    newErrors.mobileNumber = "Mobile Number is not valid.";
+    isValid = false;
+  } else {
+    newErrors.mobileNumber = "";
+  }
+
+  if (!newDevice.lastConnection) {
+    newErrors.lastConnection = "Last Connection is required.";
+    isValid = false;
+  } else {
+    newErrors.lastConnection = "";
+  }
+
+  if (!latitudeRegex.test(newDevice.latitude)) {
+    newErrors.latitude = "Latitude is not valid.";
+    isValid = false;
+  } else {
+    newErrors.latitude = "";
+  }
+
+  if (!longitudeRegex.test(newDevice.longitude)) {
+    newErrors.longitude = "Longitude is not valid.";
+    isValid = false;
+  } else {
+    newErrors.longitude = "";
+  }
+
+  return { newErrors, isValid };
+};
+
+const mobileNumberRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+const latitudeRegex = /^([-+]?(?:90(?:\.0{1,6})?|[1-9]?[0-9](?:\.\d{1,6})?))$/;
+const longitudeRegex =
+  /^([-+]?(?:180(?:\.0{1,6})?|[1-9]?[0-9]{1,2}(?:\.\d{1,6})?))$/;
+
+export default validateForm;
